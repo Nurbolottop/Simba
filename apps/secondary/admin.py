d7cba6da1e8f7d86545f750858820f79b3eb44bf
Kudbuhon08 <qweimxs@gmail.com>
2024-01-03 03:12:48 +0600

    update 5

--- apps/secondary/admin.py
+++ apps/secondary/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from apps.secondary.models import About
+from apps.secondary.models import About, Faqs
 # Register your models here.
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'image']
+
+@admin.register(Faqs)
+class FaqsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'question', 'answer']
--- apps/secondary/models.py
+++ apps/secondary/models.py
@@ -12,3 +12,17 @@ class About(models.Model):
     class Meta:
         verbose_name = "О нас"
         verbose_name_plural = "О нас"
+
+
+class Faqs(models.Model):
+    title = models.CharField(max_length = 100, verbose_name = "название")
+    description = models.CharField(max_length=155, verbose_name = "описание")
+    question = models.CharField(max_length = 155, verbose_name = "Вопрос")
+    answer = models.TextField(verbose_name = "Ответ")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
